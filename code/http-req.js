e1849e20d0a6a85b776fc0135bb36ca9a3fe42a0
magicly <magicly007@gmail.com>
2019-10-16 11:21:40 +0800

    6-crawler

--- code/http-req.js
+++ code/http-req.js
@@ -2,7 +2,8 @@ const http = require("http");
 const https = require("https");
 
 // http.get("http://localhost:7777/data.txt", res => {
-https.get("https://www.runoob.com/nodejs/nodejs-fs.html", res => {
+// https.get("https://www.runoob.com/nodejs/nodejs-fs.html", res => {
+http.get("http://www.mmonly.cc/tag/br/", res => {
   let body = [];
   res
     .on("data", chunk => {
@@ -11,6 +12,16 @@ https.get("https://www.runoob.com/nodejs/nodejs-fs.html", res => {
     })
     .on("end", () => {
       body = Buffer.concat(body).toString();
-      console.log(body);
+
+      const idx = body.indexOf('<div class="item_t">');
+      console.log("idx: ", idx);
+      let str = body.slice(idx);
+      const idx2 = str.indexOf('src="');
+      console.log("idx2: ", idx2);
+      str = str.slice(idx2 + 5);
+      const idx3 = str.indexOf(".jpg");
+      console.log("idx3: ", idx3);
+      const imgSrc = str.slice(0, idx3) + ".jpg";
+      console.log(imgSrc);
     });
 });
